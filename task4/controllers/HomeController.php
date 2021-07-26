7505b78b466a5f8bdc4a1a68af459766afd46cb7
Rost <wr2st12@gmail.com>
2021-07-26 09:02:38 +0300

    work with MVC

--- task4/controllers/HomeController.php
+++ task4/controllers/HomeController.php
@@ -1,17 +1,18 @@
 <?php
-
+include "repository/ArticleRepository.php";
 class HomeController
 {
     public function __construct()
     {
-        $this->db = new PDO('mysql:host=localhost;dbname=task4', 'root', 'qwer1234');
+        $this->pdo = new ArticleRepository;
+        $this->db = $this->pdo->get();
     }
 
     public function default()
     {
         $stmt = $this->db->query("SELECT * FROM `articles`");
         $articles = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        return $articles;
+        include "pages/home.php";
 
     }
 }
--- task4/index.php
+++ task4/index.php
@@ -2,5 +2,5 @@
 include "./controllers/HomeController.php";
 $constolers = new HomeController;
 $articles = $constolers->default();
-include "./pages/home.php";
-?>
+
+
